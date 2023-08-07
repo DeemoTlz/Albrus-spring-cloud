19de984406d5ba93d2e568dad74a7e5d04d49232
Deemo <qijiahu@example.com>
2023-08-07 22:14:30 +0800

    day4: eureka server 集群模块

---
+++ cloud-eureka-server7002/src/main/java/com/albrus/cloud/eureka/server/AlbrusCloudEurekaServer7002Application.java
@@ -0,0 +1,15 @@
+package com.albrus.cloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class AlbrusCloudEurekaServer7002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbrusCloudEurekaServer7002Application.class);
+    }
+
+}
