4e32bf771e2458fc5c97741fa7a66ed7da3da3e9
Deemo <qijiahu@example.com>
2023-08-11 19:24:11 +0800

    day5: order 模块修改为集群调用方式时，默认并不能认识服务名称，需要使用 @LoadBalanced 注解赋予 RestTemplate 负载均衡的能力

--- cloud-consumer-order80/src/main/java/com/albrus/cloud/order/config/AlbrusCloudConsumerOrder80Config.java
+++ cloud-consumer-order80/src/main/java/com/albrus/cloud/order/config/AlbrusCloudConsumerOrder80Config.java
@@ -1,5 +1,6 @@
 package com.albrus.cloud.order.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AlbrusCloudConsumerOrder80Config {
 
+
+    /**
+     * 使用 @LoadBalanced 注解赋予 RestTemplate 负载均衡的能力
+     */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
