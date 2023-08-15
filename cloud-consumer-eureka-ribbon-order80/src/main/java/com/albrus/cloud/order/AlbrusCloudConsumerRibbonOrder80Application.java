c1c25adfdf7c66afcdca3cb8822d6884aec300b6
Deemo <qijiahu@example.com>
2023-08-15 22:41:09 +0800

    day7: 代码维护（统一命名风格 AlbrusCloudConsumerRibbonOrder80Application）

--- cloud-consumer-eureka-ribbon-order80/src/main/java/com/albrus/cloud/order/AlbrusCloudConsumerRibbonOrder80Application.java
+++ cloud-consumer-eureka-ribbon-order80/src/main/java/com/albrus/cloud/order/AlbrusCloudConsumerRibbonOrder80Application.java
@@ -9,10 +9,10 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 @EnableEurekaClient
 @SpringBootApplication
 @RibbonClient(name = "ALBRUS-CLOUD-PAYMENT-SERVICE", configuration = RibbonConfiguration.class)
-public class AlbrusCloudConsumerOrderRibbon80Application {
+public class AlbrusCloudConsumerRibbonOrder80Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlbrusCloudConsumerOrderRibbon80Application.class, args);
+        SpringApplication.run(AlbrusCloudConsumerRibbonOrder80Application.class, args);
     }
 
 }
